971624c972da83d637bd4843a30dc0bd9ade8867
Luzi82 <luzi82@gmail.com>
2017-02-10 23:57:28 +0800

    run all cs

---
+++ all_cs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 -u tournament-cs0.py > t0.log
+python3 -u tournament-cs2.py > t2.log
+python3 -u tournament-cs3.py > t3.log
+python3 -u tournament-cs4.py > t4.log
+python3 -u tournament-cs5.py > t5.log
